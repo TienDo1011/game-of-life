823f3938635a7b01ea063c1fceb0f6ef79f2f6f1
Tien Do <tiendo1011@gmail.com>
2017-12-05 00:44:00 +0700

    Deploy

--- src/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const TOGGLE_ALIVE = 'TOGGLE_ALIVE';
-export const RANDOMIZE = 'RANDOMIZE';
-export const CLEAR = 'CLEAR';
-export const TOGGLE_PLAY = 'TOGGLE_PLAY';
-export const NEXTTICK = 'NEXTTICK';
